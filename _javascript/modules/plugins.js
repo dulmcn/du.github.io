a9ea3b830f24701c9926091895d654a6b151c5fc
dulmcn <63831080+dulmcn@users.noreply.github.com>
2024-11-09 13:44:46 +0800

    Update plugins.js
    Signed-off-by: dulmcn <63831080+dulmcn@users.noreply.github.com>

--- _javascript/modules/plugins.js
+++ _javascript/modules/plugins.js
@@ -4,3 +4,4 @@ export { loadImg } from './components/img-loading';
 export { imgPopup } from './components/img-popup';
 export { initLocaleDatetime } from './components/locale-datetime';
 export { initToc } from './components/toc';
+<script src="https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js"></script>
