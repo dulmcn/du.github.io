a00f141b7dbd9797b9fafad18ee4c0065299683f
dulmcn <63831080+dulmcn@users.noreply.github.com>
2024-12-07 14:28:21 +0800

    Update theme.js
    Signed-off-by: dulmcn <63831080+dulmcn@users.noreply.github.com>

--- _javascript/modules/theme.js
+++ _javascript/modules/theme.js
@@ -8,18 +8,22 @@ class Theme {
   static #modeAttr = 'data-mode';
   static #darkMedia = window.matchMedia('(prefers-color-scheme: dark)');
   static switchable = !document.documentElement.hasAttribute(this.#modeAttr);
+
   static get DARK() {
     return 'dark';
   }
+
   static get LIGHT() {
     return 'light';
   }
+
   /**
    * @returns {string} Theme mode identifier
    */
   static get ID() {
     return 'theme-mode';
   }
+
   /**
    * Gets the current visual state of the theme.
    *
@@ -33,18 +37,23 @@ class Theme {
       return this.#sysDark ? this.DARK : this.LIGHT;
     }
   }
+
   static get #mode() {
     return sessionStorage.getItem(this.#modeKey);
   }
+
   static get #isDarkMode() {
     return this.#mode === this.DARK;
   }
+
   static get #hasMode() {
     return this.#mode !== null;
   }
+
   static get #sysDark() {
     return this.#darkMedia.matches;
   }
+
   /**
    * Maps theme modes to provided values
    * @param {string} light Value for light mode
@@ -57,6 +66,7 @@ class Theme {
       [this.DARK]: dark
     };
   }
+
   /**
    * Initializes the theme based on system preferences or stored mode
    */
@@ -64,22 +74,27 @@ class Theme {
     if (!this.switchable) {
       return;
     }
+
     this.#darkMedia.addEventListener('change', () => {
       const lastMode = this.#mode;
       this.#clearMode();
+
       if (lastMode !== this.visualState) {
         this.#notify();
       }
     });
+
     if (!this.#hasMode) {
       return;
     }
+
     if (this.#isDarkMode) {
       this.#setDark();
     } else {
       this.#setLight();
     }
   }
+
   /**
    * Flips the current theme mode
    */
@@ -91,18 +106,22 @@ class Theme {
     }
     this.#notify();
   }
+
   static #setDark() {
     document.documentElement.setAttribute(this.#modeAttr, this.DARK);
     sessionStorage.setItem(this.#modeKey, this.DARK);
   }
+
   static #setLight() {
     document.documentElement.setAttribute(this.#modeAttr, this.LIGHT);
     sessionStorage.setItem(this.#modeKey, this.LIGHT);
   }
+
   static #clearMode() {
     document.documentElement.removeAttribute(this.#modeAttr);
     sessionStorage.removeItem(this.#modeKey);
   }
+
   /**
    * Notifies other plugins that the theme mode has changed
    */
@@ -110,5 +129,7 @@ class Theme {
     window.postMessage({ id: this.ID }, '*');
   }
 }
+
 Theme.init();
+
 export default Theme;
